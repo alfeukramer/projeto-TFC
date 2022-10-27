b07ead4ae3e89483ec0c0c1d054a47dc437eff41
alfeukramer <alfeukramer@gmail.com>
2022-10-27 16:00:43 -0300

    Requisito 01

---
+++ app/backend/src/database/migrations/20221027184906-01-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+     await queryInterface.createTable('users', { 
+    id: {
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+  });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   await queryInterface.dropTable('users');
+
+  }
+};
